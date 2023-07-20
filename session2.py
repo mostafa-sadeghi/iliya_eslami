7356cbcf367c9db2b5a64b5a1357bcf910bd30f5
msa <msai.sadehi@gmail.com>
2023-07-20 20:56:21 +0330

    sadeghi

---
+++ session2.py
@@ -0,0 +1,56 @@
+# print(1 + 2)
+# print("1+2")
+# print("hello every body")
+#
+# print(1 * 2 - 2)
+#
+# print(2 + 3 * 2)
+# print((2 + 3) * 2)
+#
+# print(4/2)
+
+
+# x = 12
+# print(x)
+# name = "iliya"
+# print("hello", name)
+#
+# number1 = 15
+
+
+# x = 5
+# y = 6
+# z = 2
+#
+#
+# print(x + y - z)
+
+
+# message = """hello every body
+# welcome to python class
+# we are going to learn every things about python"""
+# print(message)
+# message = "hello every body \nwelcome to python class \nwe are going to learn every things about python"
+# print(message)
+
+
+# message = 'he said "something"'
+# print(message)
+#
+# message = "he said \"something\""
+# print(message)
+
+# name = input("Enter your name: ")
+# # print("hello", name)
+# message = f"hello {name}"
+# print(message)
+
+
+name = input("enter a name: ")
+family = input("enter a family: ")
+
+message = name + " " + family
+print(message)
+message = f"{name} {family}"
+print(message)
+print(name, family)
